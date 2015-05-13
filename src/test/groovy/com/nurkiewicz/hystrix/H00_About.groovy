6f88d58b77012c0aa1bfba9dccae80851f0c9c37
Tomasz Nurkiewicz <nurkiewicz@gmail.com>
2015-05-13 22:56:46 +0200

    Asciiart

--- src/test/groovy/com/nurkiewicz/hystrix/H00_About.groovy
+++ src/test/groovy/com/nurkiewicz/hystrix/H00_About.groovy
@@ -1,14 +1,17 @@
 package com.nurkiewicz.hystrix
 
 /**
+ *     __ __         __      _
+ *    / // /_ _____ / /_____(_)_ __
+ *   / _  / // (_-</ __/ __/ /\ \ /
+ *  /_//_/\_, /___/\__/_/ /_//_\_\
+ *       /___/
  *
  * Hystix - managing failures in distrbuted systems
  *
  * Tomasz Nurkiewicz
  *
- * @tnurkiewicz
- *
- * I work for @4financeit
+ * @tnurkiewicz | @4financeit
  *
  * www.nurkiewicz.com
  *
