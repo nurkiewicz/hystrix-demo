929b1c8cffa3019a62fef79858b9df52fb12280e
Tomasz Nurkiewicz <nurkiewicz@gmail.com>
2015-05-18 19:57:33 +0200

    Initial slide

---
+++ src/test/groovy/com/nurkiewicz/hystrix/H00_About.groovy
@@ -0,0 +1,23 @@
+package com.nurkiewicz.hystrix
+
+/**
+ *     __ __         __      _
+ *    / // /_ _____ / /_____(_)_ __
+ *   / _  / // (_-</ __/ __/ /\ \ /
+ *  /_//_/\_, /___/\__/_/ /_//_\_\
+ *       /___/
+ *
+ * Hystrix - managing failures in distributed systems
+ *
+ * Tomasz Nurkiewicz
+ *
+ * @tnurkiewicz | @4financeit
+ *
+ * www.nurkiewicz.com
+ *
+ * wwww.github.com/nurkiewicz/hystrix-demo
+ *
+ */
+class H00_About {
+
+}
